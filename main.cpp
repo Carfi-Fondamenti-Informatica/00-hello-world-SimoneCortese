16052f27a5e887c27762e245bfafdaf949db8345
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-11-11 13:01:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    cout<<"Hello, World!"<<endl;
+    //Qui si definiscono le variabili
+    float a, b, c;
+    cin >> a >> b >> c;
+
+    //Scriviamo il codice necessario per eseguire i calcoli richiesti
+    float AreaTriangolo = (a + b) / 2;
+    float AreaQuadrato = a * a;
+    float AreaRettangolo = a * b;
+    float AreaTrapezio = ((a+b) * c) / 2;
+    cout << AreaTriangolo << endl;
+    cout << AreaQuadrato << endl;
+    cout << AreaRettangolo << endl;
+    cout << AreaTrapezio << endl;
+
+    return 0;
 }
